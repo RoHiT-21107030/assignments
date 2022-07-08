c020293b4ff40cb733a0603b404e84cfa1d008c7
RoHiT-21107030 <104916352+RoHiT-21107030@users.noreply.github.com>
2022-07-08 23:58:30 +0530

    Update Assignment-6 (21107030).py

--- Assignment-6 (21107030).py
+++ Assignment-6 (21107030).py
@@ -74,25 +74,25 @@ print()
 
 # Q8
 def findTriplets(arr, n):
-    found = False
-    for i in range(0, n - 2):
 
-        for j in range(i + 1, n - 1):
+	found = False
+	for i in range(0, n-2):
+	
+		for j in range(i+1, n-1):
+		
+			for k in range(j+1, n):
+			
+				if (arr[i] + arr[j] + arr[k] == 0):
+					print(arr[i], arr[j], arr[k])
+					found = True
+	
+	if (found == False):
+		print(" not exist ")
 
-            for k in range(j + 1, n):
 
-                if (arr[i] + arr[j] + arr[k] == 0):
-                    print(arr[i], arr[j], arr[k])
-                    found = True
-    if (found == False):
-        print("No triplets exist")
-lst = []
-n = int(input("Enter number of elements : "))
-
-for i in range(0, n):
-    ele = int(input())
-    lst.append(ele)
-print(findTriplets(lst, n))
+arr = [0, -1, 2, -3, 1]
+n = len(arr)
+findTriplets(arr, n)
 
 # Q9
 class parantheses:
